f6514fcce4e089d7e61c05a168514a786bdb2b13
Akshay Patil <akshaypatil1920@gmail.com>
2023-07-16 13:07:51 +0530

    Static and non static methods

---
+++ src/methods/StaticAndNonStaticmethods.java
@@ -0,0 +1,19 @@
+package methods;
+
+public class StaticAndNonStaticmethods {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	//This method is non static method
+	public void method1() {
+		System.out.println("This is method1");
+	}
+	
+	//This method is static method
+	public static void method2() {
+		System.out.println("This is method2 and this is static");
+	}
+
+}
