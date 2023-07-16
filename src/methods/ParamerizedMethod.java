e9506e74b096b8221d4f66bd22b818e0d86b1b20
Akshay Patil <akshaypatil1920@gmail.com>
2023-07-16 14:31:55 +0530

    paramerized method

---
+++ src/methods/ParamerizedMethod.java
@@ -0,0 +1,26 @@
+package methods;
+
+public class ParamerizedMethod {
+
+	public static void main(String[] args) {
+	
+
+	}
+
+}
+
+class method1 {
+
+	public void magic(int a, int b) {
+
+	}
+
+	public static void magic1(int a, int b) {
+
+	}
+
+	public void magic2(String b, double rr) {
+
+	}
+
+}
