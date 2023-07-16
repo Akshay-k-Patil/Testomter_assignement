8436b61f82a648072f5bf0f3b4c902c2f5b0afff
Akshay Patil <akshaypatil1920@gmail.com>
2023-07-16 13:32:30 +0530

    void methods with static and non static

---
+++ src/methods/CreateVoidMethod.java
@@ -0,0 +1,18 @@
+package methods;
+
+public class CreateVoidMethod {
+	
+	
+	//Create any void method
+	//create 2 methods one should be static and one non static method
+	
+	public void magic() {
+		System.out.println("This is magic method");
+		
+	}
+	
+	public static void magic2() {
+		System.out.println("This is magic2 methos");
+	}
+
+}
