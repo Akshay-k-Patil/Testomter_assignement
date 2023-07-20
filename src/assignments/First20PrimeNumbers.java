def30046d5f8f5a6da30f5456f542ac7112e2d2c
Akshay Patil <akshaypatil1920@gmail.com>
2023-07-20 15:09:34 +0530

    Write Code to get first 20 prime number?

---
+++ src/assignments/First20PrimeNumbers.java
@@ -0,0 +1,29 @@
+package assignments;
+
+public class First20PrimeNumbers {
+
+	static boolean isPrime(int num) {
+
+		if (num == 1 || num == 0)
+			return false;
+
+		for (int i = 2; i <= num / 2; i++) {
+
+			if (num % i == 0)
+				return false;
+
+		}
+		return true;
+
+	}
+
+	public static void main(String[] args) {
+		int num = 80;
+		for (int i = 1; i <= num; i++) {
+			if (isPrime(i))
+				System.out.print(i + " ");
+
+		}
+	}
+
+}
